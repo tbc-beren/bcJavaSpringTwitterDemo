af10063e8e8b4200f3a1f526b32598d162ec2bd0
tbc-beren <beren.themoonlightshade@gmail.com>
2021-07-03 13:47:57 +0200

    Project scaffolding: Spring and H2 database

---
+++ src/main/java/com/blackcodex/demo/spring/twitter/BCSpringTwitterDemo.java
@@ -0,0 +1,11 @@
+package com.blackcodex.demo.spring.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BCSpringTwitterDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(BCSpringTwitterDemo.class, args);
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO TWEETS (username, text) VALUES
+  ('@TweeterDav', 'The Twitter Developer Platform. Ooh la la! https://developer.twitter.com');
